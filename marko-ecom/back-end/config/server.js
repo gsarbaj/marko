9a3bf75b939766e01fba1e8e9a6ba16aabb58d97
Genrich Sarbaj <gsarbaj@Genrichs-Mac-mini.local>
2021-11-16 09:19:13 +0200

    Deployed on heroku

--- marko-ecom/back-end/config/server.js
+++ marko-ecom/back-end/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1339),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6e5c519b2d2cc7a7477f0639071a45f0'),
